8cd74f897fa2aa3b735d45812750a9ed4ce638fb
JarmoKu <jarmo.august.kuutti@gmail.com>
2023-01-31 12:56:39 +0200

    Removed deprecated scripts

--- Assets/Tweening/Extensions/TweenExtensions.cs
+++ Assets/Tweening/Extensions/TweenExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace JK.Tweening
 {
@@ -175,6 +176,55 @@ namespace JK.Tweening
         public static Vector3 GetRotation (this Transform transform, Space space)
         {
             return space.Matches (Space.World) ? transform.eulerAngles : transform.localEulerAngles;
+        }
+        #endregion
+
+        #region Image
+        public static ImageColorTween ColorFromTo (this Image image, Color start, Color end, float duration)
+        {
+            return new ImageColorTween (image, start, end, duration);
+        }
+
+        public static ImageColorTween ColorFrom (this Image image, Color start, float duration)
+        {
+            return new ImageColorTween (image, start, image.color, duration);
+        }
+
+        public static ImageColorTween ColorTo (this Image image, Color end, float duration)
+        {
+            return new ImageColorTween (image, image.color, end, duration);
+        }
+
+        public static ImageColorTween ColorThroughGradient (this Image image, Gradient gradient, float duration)
+        {
+            return new ImageColorTween (image, gradient, duration);
+        }
+
+        public static ImageColorTween AlphaFromTo (this Image image, float start, float end, float duration)
+        {
+            var startColor = image.color;
+            startColor.a = start;
+
+            var endColor = image.color;
+            endColor.a = end;
+
+            return new ImageColorTween (image, startColor, endColor, duration);
+        }
+
+        public static ImageColorTween AlphaFrom (this Image image, float start, float duration)
+        {
+            var startColor = image.color;
+            startColor.a = start;
+
+            return new ImageColorTween (image, startColor, image.color, duration);
+        }
+
+        public static ImageColorTween AlphaTo (this Image image, float end, float duration)
+        {
+            var endColor = image.color;
+            endColor.a = end;
+
+            return new ImageColorTween (image, image.color, endColor, duration);
         }
         #endregion
 
